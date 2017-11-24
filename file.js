a4dca8e2c0b039afb5ce45b5a8d90175d7525b3b
PROB8 <blackb8r@gmail.com>
2017-11-24 09:41:55 -0500

    added getMiddle algorithm

--- file.js
+++ file.js
@@ -7,5 +7,20 @@ const multiply = function (a, b) {
     else{return 'Please enter valid numbers' }
 }
   
-
 module.exports = multiply;
+
+
+const getMiddle = function (word) {
+    let length = word.length
+    if (length % 2 === 1) {
+       return word.charAt(Math.floor(length/2)) 
+    }
+    else if (length % 2 === 0) { 
+        let middleOne = length/2;
+        let middleTwo = (length/2) - 1;
+        console.log(length/2)
+    }
+}
+
+module.exports = getMiddle;
+getMiddle('coon')
