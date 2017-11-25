f2bfd439fcb0a9539c0bd9d1f77e146d1d26cc9b
PROB8 <blackb8r@gmail.com>
2017-11-25 10:08:13 -0500

    fixed multiply test

--- file.js
+++ file.js
@@ -10,17 +10,17 @@ const multiply = function (a, b) {
 module.exports = multiply;
 
 
-const getMiddle = function (word) {
-    let length = word.length
-    if (length % 2 === 1) {
-       return word.charAt(Math.floor(length/2)) 
-    }
-    else if (length % 2 === 0) { 
-        let middleOne = length/2;
-        let middleTwo = (length/2) - 1;
-        console.log(length/2)
-    }
-}
+// const getMiddle = function (word) {
+//     let length = word.length
+//     if (length % 2 === 1) {
+//        return word.charAt(Math.floor(length/2)) 
+//     }
+//     else if (length % 2 === 0) { 
+//         let middleOne = length/2;
+//         let middleTwo = (length/2) - 1;
+//         console.log(length/2)
+//     }
+// }
 
-module.exports = getMiddle;
-getMiddle('coon')
+// module.exports = getMiddle;
+// getMiddle('coon')
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 const multiply = require('./file');
 
 
-test('should return multiplied numbers', function() {
+test('should return multiplied numbers', () => {
     expect(multiply(2,3)).toBe(6)
 })
   
-test('multiply string by number, should return string', function() {
+test('multiply string by number, should return string', () => {
     expect(multiply('a',3)).toBe('Please enter valid numbers')
 })
