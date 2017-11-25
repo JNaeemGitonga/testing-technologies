11ea5e5bf651d31ca517d63d35cd0093156f92a0
PROB8 <blackb8r@gmail.com>
2017-11-25 10:15:48 -0500

    added travis file

--- file.js
+++ file.js
@@ -4,6 +4,7 @@ const multiply = function (a, b) {
     if ( typeof a === 'number' && typeof b === 'number') {
         return a * b
     }
+    
     else{return 'Please enter valid numbers' }
 }
   
