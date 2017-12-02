1564e87134591e100cf69285579a2ea49696c2bd
PROB8 <blackb8r@gmail.com>
2017-12-02 16:26:39 -0500

    added if statement

--- PeaceWorld.java
+++ PeaceWorld.java
@@ -1,13 +1,20 @@
 public class PeaceWorld {
     
         public static void main(String[] args) {
-            // Prints "Hello, World" to the terminal window.
             System.out.println("Peace, World");
 
             int x = 10;
-            int y = 50;
-
+            int y = -50;
+            String str = "look at me"; 
+            System.out.println(str);
             System.out.println(x + y);
+
+            if (x > 0 & y > 0) {
+                System.out.println(true);
+            }
+            else{
+                System.out.println("one is false");
+            }
         }
 
       
