112e52ed58c49aec9562b97f6d19b0509a50b094
PROB8 <blackb8r@gmail.com>
2017-12-02 16:59:07 -0500

    added array of strings example

---
+++ java/StringArray.java
@@ -0,0 +1,12 @@
+public class StringArray {
+
+    
+    
+    public static void main(String[] args) {
+        String[] strArr = new String[2];
+        strArr[0] = "be healthy";
+        strArr[1] = "live long and prosper";
+        System.out.println(strArr[0]);
+    } 
+    
+}
