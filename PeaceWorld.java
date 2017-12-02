ef6d555a8b608ac5d631bbb2bc363287538cf6c5
PROB8 <blackb8r@gmail.com>
2017-12-02 16:18:22 -0500

    changed name HelloWorld to PeaceWorld and added some to method

---
+++ PeaceWorld.java
@@ -0,0 +1,14 @@
+public class PeaceWorld {
+    
+        public static void main(String[] args) {
+            // Prints "Hello, World" to the terminal window.
+            System.out.println("Peace, World");
+
+            int x = 10;
+            int y = 50;
+
+            System.out.println(x + y);
+        }
+
+      
+}
