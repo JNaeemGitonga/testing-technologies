686b15c596c4a4a797b8828cd3ac9db62cab4782
PROB8 <blackb8r@gmail.com>
2017-12-13 13:35:01 -0500

    practice with fs, added yola.js, movies.txt, deleted another.js

--- another.js
+++
@@ -1,7 +0,0 @@
-const obj = {
-    rap: 'titty boi',
-    'rock-n-roll':'Chuck Berry',
-    'r-an-b':'isley brothers'
-}
-
-module.exports.obj
--- file.js
+++ file.js
@@ -1,4 +1,6 @@
-const another = require('./another');
+const yola = require('./yola');
+const fs = require('fs');
+
 
 const multiply = function (a, b) {
     if ( typeof a === 'number' && typeof b === 'number') {
@@ -8,9 +10,6 @@ const multiply = function (a, b) {
     else{return 'Please enter valid numbers' }
 }
   
-
-
-
 const getMiddle = function (word) {
     let length = word.length
     if (length % 2 === 1) {
@@ -36,11 +35,33 @@ function functionThatDoesNothin(cat, dog) {
 function cb(data){
     console.log(data)
 }
-console.log('line 39',getMiddle('asssasasasasTTasasasasas'))
-console.log('line 40',process.argv[0] + process.argv[1]);
-console.log('It does nothing')
-console.log(process.argv)
+// console.log('line 39',getMiddle('asssasasasasTTasasasasas'))
+// console.log('line 40',process.argv[0] + process.argv[1]);
+// console.log('It does nothing')
+// console.log(process.argv)
 // console.log('line 43',parseFloat(process.argv[0]) + parseFloat(process.argv[1]));
+// yola.loop()
+console.log(yola.artist)
+
+const movies = fs.readFile('./movies.txt', 'utf8', (error,data) => {
+    if(error) console.log(error)
+    else{ 
+        console.log( data.split(" ") );
+        let dataArr = data.split(' ');
+        dataArr.forEach(word => console.log('these words: ', word) )
+    }
+})
+
+// fs.writeFile('food.txt', 'I made a food file', err => {
+//     if (err) console.log(err)
+//     else {fs.readFile('food.txt', 'utf8', (err,data) => {
+//         if (err) console.log(err)
+//         else {console.log(data)}
+//     })}
+// })
+// console.log('LOOK MY PROCESS: ', process)
+
+console.log('LOOK EXPONENTIATION: ', 3 ** 5)
 
 module.exports = getMiddle;
 module.exports = multiply;
