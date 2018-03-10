e7a56e41d3b2702ad98ec84ffd43aa671a4baa04
PROB8 <blackb8r@gmail.com>
2018-03-10 12:11:10 -0500

    express server added, middleware examples

---
+++ express/server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use((req,res,next) => {
+    
+    let date = new Date().toString()
+    let log = {
+        date,
+        method:req.method,
+        url:req.url
+    }
+    
+    fs.appendFile('server.log', JSON.stringify(log) + '\n', err => {
+        if(err) console.log(err)
+    })
+    next()
+})
+/*
+    the middleware below will stop all request and render the maintainence page
+        because next() isn't called.
+    KNOW: middleware is excecuted in the order that you call app.use()
+        so in the case of localhost:3000/page.html if you want it not render when visited
+        you will need to place  line 34 under the maintainence app.use()  
+
+*/
+app.use((req,res,next) => {
+    res.sendFile(path.join(__dirname + '/public/maintainence.html'))
+})
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req,res) => {
+    res.send({
+        name:'Jaha',
+        profession:'software engineer'
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.send('<h2>About Page</h2>')
+})
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: 'shit got fucked up'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+})
